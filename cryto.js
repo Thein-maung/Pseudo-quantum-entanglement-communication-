475f8896626e70ea31eefd0486f6489f25b69829
Thein-maung <59556712+Thein-maung@users.noreply.github.com>
2025-11-10 14:26:39 +0630

    Refactor formatting in cryto.js

--- cryto.js
+++ cryto.js
@@ -3,17 +3,17 @@ import { loadTwin, infer } from './twinnet.js';
 let TWIN_SEED;
 let COUNTER = 0;
 
-export async function setSeed(seed) {
+export async function setSeed(seed){
   const hash = await crypto.subtle.digest('SHA-256', seed);
   TWIN_SEED = new Uint8Array(hash);
   COUNTER = 0;
 }
 
-export async function nextPad(len) {
-  if (!TWIN_SEED) throw 'No seed set';
+export async function nextPad(len){
+  if(!TWIN_SEED) throw 'No seed set';
   await loadTwin();
   const input = TWIN_SEED.concat(new Uint8Array([COUNTER]));
   const pad = infer(input);
   COUNTER++;
-  return new Uint8Array(pad).slice(0, len);
+  return new Uint8Array(pad).slice(0,len);
 }
